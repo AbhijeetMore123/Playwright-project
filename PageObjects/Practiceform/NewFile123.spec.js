3507351fb1446f5148cfcaff00963f6f8b19612d
AbhijeetMore123 <abhijeetmore400@gmail.com>
2026-04-12 12:22:10 +0530

    Gitproject

--- PageObjects/Practiceform/NewFile123.spec.js
+++ PageObjects/Practiceform/NewFile123.spec.js
@@ -1,9 +1,10 @@
 import{test,expect} from '@playwright/test';
-test('select female gender',async({page}) => {
+
+test('select 123 female gender', async ({page}) => {
 await page.goto('https://testautomationpractice.blogspot.com/');
 await page.getByLabel('Female').check();
-await page.getByRole('radio',{name: 'Female'}).check();
-await expect(page.getByLabel('Female')).tobeChecked();
+const check= await page.getByRole('radio',{name: 'Female'}).check();
+await expect(check).toBeChecked();
 
 
 
